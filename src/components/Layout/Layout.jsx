4cdbc646d78755a100b6062349ec192ca84d933b
Marina-Chuhlib <marinachuhlib@gmail.com>
2023-03-05 18:11:27 +0300

    Upadte

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,12 +1,9 @@
-import { Navigation } from 'components/NavBar/NavBar';
+
 import { Outlet } from 'react-router-dom';
 
 export const Layout = () => {
   return (
     <>
-      {/* <header>
-        <Navigation />
-      </header> */}
       <main>
         <Outlet />
       </main>
